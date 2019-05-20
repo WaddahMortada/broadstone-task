dc9108346a02d43883ce6d80c79187fb21b6e42d
Waddah Mortada <bear@Waddahs-MBP.lan>
2019-05-20 15:49:41 +0100

    Initialise project

---
+++ app/Application.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Application extends Model
+{
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'worker_id', 'shift_id', 'status'
+	];
+}
---
+++ app/Shift.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Shift extends Model
+{
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'from', 'to', 'application_id'
+	];
+}
