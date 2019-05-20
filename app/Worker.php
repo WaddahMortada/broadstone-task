66db6b7727bc044b9a40e8ceb095748e5b9f0b9d
Waddah Mortada <w.mortada87@gmail.com>
2019-05-20 15:59:55 +0100

    Implement Worker model

--- app/Worker.php
+++ app/Worker.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Application;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -36,4 +37,142 @@ class Worker extends Authenticatable
 	protected $casts = [
 		'email_verified_at' => 'datetime',
 	];
+
+	/**
+	 * Application relationship with Worker
+	 */
+	public function application()
+	{
+		return $this->hasMany(Application::class);
+	}
+
+	/**
+	 * Return Id
+	 *
+	 * @return string
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * Return name
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	/**
+	 * Return email address
+	 *
+	 * @return string
+	 */
+	public function getEmail()
+	{
+		return $this->email;
+	}
+
+	/**
+	 * Return email verified at
+	 *
+	 * @return string|null datetime (Y-m-d H:i:s)
+	 */
+	public function getEmailVerifiedAt()
+	{
+		return $this->email_verified_at;
+	}
+
+	/**
+	 * Return password
+	 *
+	 * @return string
+	 */
+	public function getPassword()
+	{
+		return $this->password;
+	}
+
+	/**
+	 * Return phone
+	 *
+	 * @return string
+	 */
+	public function getPhone()
+	{
+		return $this->phone;
+	}
+
+	/**
+	 * Set name
+	 *
+	 * @param string $name
+	 *
+	 * @return Worker
+	 */
+	public function setName($name)
+	{
+		$this->name = $name;
+
+		return $this;
+	}
+
+	/**
+	 * Set email address
+	 *
+	 * @param string $email
+	 *
+	 * @return Worker
+	 */
+	public function setEmail($email)
+	{
+		$this->email = $email;
+
+		return $this;
+	}
+
+	/**
+	 * Set email verified at
+	 *
+	 * @param string $emailVerifiedAt datetime (Y-m-d H:i:s)
+	 *
+	 * @return Worker
+	 */
+	public function setEmailVerifiedAt($emailVerifiedAt)
+	{
+		$this->email_verified_at = $emailVerifiedAt;
+
+		return $this;
+	}
+
+	/**
+	 * Set password
+	 *
+	 * @param string $password
+	 *
+	 * @return Worker
+	 */
+	public function setPassword($password)
+	{
+		$this->password = $password;
+
+		return $this;
+	}
+
+	/**
+	 * Set phone
+	 *
+	 * @param string $phone
+	 *
+	 * @return Worker
+	 */
+	public function setPhone($phone)
+	{
+		$this->phone = $phone;
+
+		return $this;
+	}
 }
